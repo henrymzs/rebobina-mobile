cec05d900a28cfb25e402ae0a21b8b2ff89e1a9e
Henry <henrykaua21@gmail.com>
2025-08-04 23:44:53 -0300

    feat: adiciona tela de login, com inputs para email e senha, utilizando componente de input e botao e adicionando botao para retornar a tela anterior

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,27 +1,54 @@
-import { View, Text, StyleSheet } from "react-native";
-import { router } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router"
 
 import { Button } from "@/components/button";
+import { Input } from "@/components/input";
 
 export default function Login() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Login</Text>
-            <Button title="Voltar" onPress={() => router.back()} />
+            <Pressable style={styles.backButton}>
+                <Ionicons name="arrow-back" size={24} color="#FFFFFF" 
+                onPress={() => router.back()}
+                />
+            </Pressable>
+            <Text style={styles.logo}>Rebobina</Text>
+            <View style={styles.form}>
+                <Text style={styles.title}>Faça Login</Text>
+                <Input placeholder="Seu E-mail" placeholderTextColor="#999" />
+                <Input placeholder="Sua Senha" placeholderTextColor="#999" />
+                <Button title="Fazer Login" />
+            </View>
         </View>
-    )
+    );
 }
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "#080808ff",
+        padding: 32,
+    },
+    logo: {
+        fontSize: 32,
+        fontWeight: "bold",
+        color: "#FFFFFF",
+        textAlign: "center",
+        marginTop: 48,
+    },
+    form: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        padding: 32,
         gap: 16,
     },
     title: {
         fontSize: 18,
         fontWeight: "bold",
+        color: "#FFFFFF",
     },
-})
+    backButton: {
+        paddingTop: 22
+    }
+});
