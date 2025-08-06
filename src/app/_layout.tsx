0292e247d7b3138146907210292b00127729e67d
Henry <henrykaua21@gmail.com>
2025-08-06 00:01:21 -0300

    feat: muda layout para organizacao de rotas, colocando rotas de paginas para usuario nao logado e retirando o header padrao

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="(auth)/login"
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="(auth)/register"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  )
 
 }
